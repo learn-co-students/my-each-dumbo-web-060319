cfc56a53b2ee08eddc74f2772f4c53232bcc2d58
Alexander Schelchere <ascellie@gmail.com>
2019-05-13 11:47:57 -0400

    My each - done

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
+def my_each(arr)
   # code here
+  if block_given?
+    # incremental variable
+    i = 0
+
+    while i < arr.length
+      yield(arr[i])
+      i += 1
+    end
+    arr
+  else
+    arr
+  end
 end
